f9fffef528a63700c1681c7cc9e38b7873c1153d
emrebaykusak1 <emrebaykusak12@gmail.com>
2025-02-05 22:01:14 +0300

    Initial commit

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+public record DeleteOrderResult(bool IsSuccess);
+
+public class CreateOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ordering.Infrastructure.Data;
+
+public class DesignTimeDbContextFactory :  IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        
+        optionsBuilder.UseSqlServer("Server=localhost;Database=OrderDb;User Id=sa;Password=SwN12345678;Encrypt=false;TrustServerCertificate=True");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
